13c61cacdf0fa73438b577f8656991c1137caafc
andela-ofamamkinde <olatoyosi.famakinde@andela.co>
2017-06-08 01:18:16 +0100

    Move Firebase config to file

---
+++ public/firebase.js
@@ -0,0 +1,11 @@
+(function() {
+    var config = {
+        apiKey: "API_KEY",
+        authDomain: "AUTH_DOMAIN",
+        databaseURL: "DATABASE_URL",
+        projectId: "PROJECT_ID",
+        storageBucket: "STORE_BUCKEET",
+        messagingSenderId: "MESSAGING_SENDER_ID"
+    };
+    firebase.initializeApp(config);
+})();
